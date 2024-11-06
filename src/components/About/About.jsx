d2197fe732741db4e2e546ff3f81acfadc68abee
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2024-11-05 16:01:28 -0800

    Add more fonts and bullet points to blog

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,11 @@ export default function About() {
                     or service here to give readers a better understanding.
                 </p>
                 <img
-                    src="https://via.placeholder.com/200"
+                    src="https://placehold.co/200x200?text=Dummy+Image&font=raleway"
                     alt="Content image"
                     className="md:w-1/2 w-full  mt-4 md:mt-0 rounded-lg shadow-lg"
                 />
-            </div>
+            </div> 
 
             {/* Second paragraph with image on the left */}
             <div className="flex flex-col md:flex-row-reverse items-center md:space-x-reverse md:space-x-8 pr-20 pl-20 md:p-0">
@@ -25,20 +25,20 @@ export default function About() {
                     message.
                 </p>
                 <img
-                    src="https://via.placeholder.com/200"
+                    src="https://placehold.co/200x200?text=Dummy+Image+2&font=raleway"
                     alt="Content image"
-                    className="md:w-1/2 w-full mt-4 md:mt-0 rounded-lg shadow-lg"
+                    className="md:w-1/2 w-full  mt-4 md:mt-0 rounded-lg shadow-lg"
                 />
             </div>
             {/* First paragraph with image on the right */}
             <div className="flex flex-col md:flex-row items-center md:space-x-8 pr-20 pl-20 md:p-0">
                 <p className="md:w-1/2 text-gray-700">
-                    This is the first paragraph. It can contain any information
+                    This is the third paragraph. It can contain any information
                     you want to display. You can describe your content, product,
                     or service here to give readers a better understanding.
                 </p>
                 <img
-                    src="https://via.placeholder.com/200"
+                    src="https://placehold.co/200x200?text=Dummy+Image+3&font=raleway"
                     alt="Content image"
                     className="md:w-1/2 w-full  mt-4 md:mt-0 rounded-lg shadow-lg"
                 />
