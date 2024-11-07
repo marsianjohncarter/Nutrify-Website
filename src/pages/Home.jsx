a242a3f74be01bb811276bfb5f6f683689eb84e9
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2024-11-07 09:04:20 -0800

    Add react helmet to project and also remove unused links in navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
+import { Helmet } from "react-helmet";
+
 import About from "../components/About/About";
 import Carousel from "../components/Carousel/Carousel";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
-import Footer from '../components/Footer/Footer';
-import Pricing from '../components/Pricing/Pricing';
+import Pricing from "../components/Pricing/Pricing";
 
 export default function Home() {
     return (
         <div>
+            <Helmet>
+                <title>Nutrify: Whole Food Scanner</title>
+                <meta
+                    name="description"
+                    content="Nutrify is an iOS application designed to make learning about whole foods fun and in turn, reduce the intake of ultra-processed foods. It provides personalized nutrition recommendations, tracks your daily food intake, and offers a community to share your progress with others. It is the perfect tool for anyone looking to improve their health and wellbeing. Nutrify is available on the App Store.  Whole food scanner, nutrition and calorie tracker, visual healthy food diary."
+                />
+            </Helmet>
             <Jumbotron />
             <div className="mt-10">
                 <Carousel />
@@ -15,4 +23,4 @@ export default function Home() {
             <Pricing />
         </div>
     );
-};
+}
